ab563f11de55dad2e88882174524a2f223b9d093
9013Matrix <9013.matrix@gmail.com>
2025-09-18 17:58:34 -0400

    color

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVisionColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVisionColorSensor.java
@@ -19,13 +19,13 @@
  * SOFTWARE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import android.util.Size;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -59,35 +59,12 @@ import org.firstinspires.ftc.vision.opencv.PredominantColorProcessor;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Disabled
-@TeleOp(name = "Concept: Vision Color-Sensor", group = "Concept")
-public class ConceptVisionColorSensor extends LinearOpMode
+
+public class ConceptVisionColorSensor
 {
-  @Override
-  public void runOpMode()
-  {
-    /* Build a "Color Sensor" vision processor based on the PredominantColorProcessor class.
-     *
-     * - Focus the color sensor by defining a RegionOfInterest (ROI) which you want to inspect.
-     *    This can be the entire frame, or a sub-region defined using:
-     *    1) standard image coordinates or 2) a normalized +/- 1.0 coordinate system.
-     *    Use one form of the ImageRegion class to define the ROI.
-     *      ImageRegion.entireFrame()
-     *      ImageRegion.asImageCoordinates(50, 50,  150, 150)  100x100 square at the top left corner
-     *      ImageRegion.asUnityCenterCoordinates(-0.1, 0.1, 0.1, -0.1)  10% W * H centered square
-     *
-     * - Set the list of "acceptable" color swatches (matches).
-     *     Only colors that you assign here will be returned.
-     *     If you know the sensor will be pointing to one of a few specific colors, enter them here.
-     *     Or, if the sensor may be pointed randomly, provide some additional colors that may match.
-     *     Possible choices are:
-     *         RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, MAGENTA, BLACK, WHITE
-     *     Note: For the 2026 season ARTIFACT_PURPLE and ARTIFACT_GREEN have been added.
-     *
-     *     Note that in the example shown below, only some of the available colors are included.
-     *     This will force any other colored region into one of these colors.
-     *     eg: Green may be reported as YELLOW, as this may be the "closest" match.
-     */
+  private final Telemetry telemetry;
+  private final HardwareMap hardwareMap;
+  
     PredominantColorProcessor colorSensor = new PredominantColorProcessor.Builder()
       .setRoi(ImageRegion.asUnityCenterCoordinates(-0.1, 0.1, 0.1, -0.1))
       .setSwatches(
@@ -108,7 +85,7 @@ public class ConceptVisionColorSensor extends LinearOpMode
      *      Since a high resolution will not improve this process, choose a lower resolution
      *      supported by your camera.  This will improve overall performance and reduce latency.
      *  - Choose your video source.  This may be
-     *      .setCamera(hardwareMa1p.get(WebcamName.class, "Webcam 1"))  .....   for a webcam
+     *      .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))  .....   for a webcam
      *  or
      *      .setCamera(BuiltinCameraDirection.BACK)    ... for a Phone Camera
      */
@@ -154,5 +131,5 @@ public class ConceptVisionColorSensor extends LinearOpMode
       
       sleep(20);
     }
-  }
+  
 }
