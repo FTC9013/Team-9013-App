9bbfcd27568f52c6221f15628e2db16ccf7b0fd5
9013Matrix <9013.matrix@gmail.com>
2025-10-27 17:13:10 -0400

    Me, Ryan complete Red auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DacodAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DacodAuto.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public abstract class DacodAuto extends LinearOpMode
@@ -15,33 +17,17 @@ public abstract class DacodAuto extends LinearOpMode
   public ConceptVisionColorSensor conceptVisionColorSensor;
   
   Pose2d LAUNCH_POSITION = new Pose2d(-40.25, 15.5, Math.toRadians(-45));
-  Pose2d SPIKE1 = new Pose2d(-11.25, 31, Math.toRadians(90));
-  Pose2d SPIKE2 = new Pose2d(12, 31, Math.toRadians(90));
-  Pose2d SPIKE3 = new Pose2d(35, 31, Math.toRadians(90));
+  Pose2d SPIKE_GPP = new Pose2d(-11.25, 31, Math.toRadians(90));
+  Pose2d SPIKE_PGP = new Pose2d(12, 31, Math.toRadians(90));
+  Pose2d SPIKE_PPG = new Pose2d(35, 31, Math.toRadians(90));
   Pose2d SCANNING_POINT = new Pose2d(-31.25, 11.5, Math.toRadians(0));
   Double INTAKE = 45.0;
   Double BACK_UP = 31.0;
   Pose2d STARTING1 = new Pose2d(-61.25, 11.5, 0);
   Pose2d STARTING2 = new Pose2d(-56, 50, Math.toRadians(-45));
+  Vector2d OUT_OF_LAUNCH = new Vector2d(-16, 38);
   
-  /*
-  md.runAction(md.getDrive().actionBuilder(STARTING2)
-    .splineToLinearHeading(SCANNING_POINT, Math.toRadians(0))
-    .splineToLinearHeading(LAUNCH_POSITION, LAUNCH_POSITION.heading)
-    .splineToLinearHeading(SPIKE1, SPIKE1.heading)
-      .lineToY(INTAKE)
-      .splineToLinearHeading(LAUNCH_POSITION, LAUNCH_POSITION.heading)
-      .splineToLinearHeading(SPIKE2, SPIKE2.heading)
-      .lineToY(INTAKE)
-      .lineToY(BACK_UP)
-      .splineToLinearHeading(LAUNCH_POSITION, LAUNCH_POSITION.heading)
-      .splineToLinearHeading(SPIKE3, SPIKE3.heading)
-      .lineToY(INTAKE)
-      .lineToY(BACK_UP)
-      .splineToLinearHeading(LAUNCH_POSITION, LAUNCH_POSITION.heading)
-      .strafeTo(new Vector2d(-16, 38))
-  .build());
-  */
+  
   public Pose2d adjust(Pose2d pose)
   
   {
@@ -67,68 +53,77 @@ public abstract class DacodAuto extends LinearOpMode
     telemetry.update();
     
     //4 paths and actions
-    Action MoveToScanning = robot.actionBuilder(getStartingPose())
-        .splineToLinearHeading(adjust(SCANNING_POINT), Math.toRadians(0)).build();
+    Action moveToScanning = robot.actionBuilder(getStartingPose())
+      .splineToLinearHeading(adjust(SCANNING_POINT), Math.toRadians(0)).build();
     
     
-    Action GotoSpikes = robot.actionBuilder(adjust(SCANNING_POINT))
+    Action launchPreloaded = robot.actionBuilder(adjust(SCANNING_POINT))
+      //preloaded
       .splineToLinearHeading(LAUNCH_POSITION, LAUNCH_POSITION.heading)
-      .splineToLinearHeading(SPIKE1, SPIKE1.heading)
+      .build();
+    
+    Action gotoSpikeGPP = robot.actionBuilder(adjust(LAUNCH_POSITION))
+      //spike GPP
+      .splineToLinearHeading(SPIKE_GPP, SPIKE_GPP.heading)
       .lineToY(INTAKE)
       .splineToLinearHeading(LAUNCH_POSITION, LAUNCH_POSITION.heading)
-      .splineToLinearHeading(SPIKE2, SPIKE2.heading)
+      .build();
+    
+    Action gotoSpikePGP = robot.actionBuilder(adjust(LAUNCH_POSITION))
+      //spike PGP
+      .splineToLinearHeading(SPIKE_PGP, SPIKE_PGP.heading)
       .lineToY(INTAKE)
       .lineToY(BACK_UP)
       .splineToLinearHeading(LAUNCH_POSITION, LAUNCH_POSITION.heading)
-      .splineToLinearHeading(SPIKE3, SPIKE3.heading)
+      .build();
+    
+    Action gotoSpikePPG = robot.actionBuilder(adjust(LAUNCH_POSITION))
+      //spike PPG
+      .splineToLinearHeading(SPIKE_PPG, SPIKE_PPG.heading)
       .lineToY(INTAKE)
       .lineToY(BACK_UP)
       .splineToLinearHeading(LAUNCH_POSITION, LAUNCH_POSITION.heading)
-      .strafeTo(new Vector2d(-16, 38))
-      .build());
-      
-
-
+      .build();
+    
+    Action getOut = robot.actionBuilder(adjust(LAUNCH_POSITION))
+      // out of launch_position
+      .strafeTo(OUT_OF_LAUNCH)
+      .build();
+    
+    
     waitForStart();
+    Actions.runBlocking(moveToScanning);
     Motif motifPattern = aprilTagCamera.detectAprilTag();
     
     
-    //the auto method
-    //findObelisk();
-//    aprilTagCamera.detectAprilTag();//we need to improve on this line later
-    launchArtifacts();
-    collectArtifacts(aprilTagCamera.detectAprilTag());
-    launchArtifacts();
-    collectArtifacts(aprilTagCamera.detectAprilTag());
-    launchArtifacts();
+    //go to the spike marks and collect artifacts
+    if (motifPattern == Motif.GPP)
+    {
+      Actions.runBlocking(
+        new SequentialAction(
+          gotoSpikeGPP, gotoSpikePGP, gotoSpikePPG));
+      
+    } else if (motifPattern == Motif.PGP)
+    {
+      Actions.runBlocking(
+        new SequentialAction(
+          gotoSpikePGP, gotoSpikePPG, gotoSpikeGPP));
+      
+    } else if (motifPattern == Motif.PPG)
+    {
+      Actions.runBlocking(
+        new SequentialAction(
+          gotoSpikePPG, gotoSpikePGP, gotoSpikeGPP));
+    }
+    Actions.runBlocking(getOut);
   }
   
-  public void launchPreloaded()
-  {
-  
-  }
   
   public abstract Pose2d getStartingPose();
   
-  public void collectArtifacts(Motif desiredSpike)
-  {
-    //go to the spike marks and collect artifacts
-    if (desiredSpike == Motif.GPP)
-    {
-    
-    } else if (desiredSpike == Motif.PPG)
-    {
-    
-    } else if (desiredSpike == Motif.PGP)
-    {
-    
-    }
-    
-    
-  }
   
   /*
-   Auto routine:z
+   Auto routine:
    call findObelisk()
    do something with the aprilTagCamera.detectAprilTag() method
    call goToLaunchPosition()
@@ -138,11 +133,5 @@ public abstract class DacodAuto extends LinearOpMode
    call launcher.startLaunching()
    repeat up 3 steps above up to 3 times
   */
-  public void launchArtifacts()
-  {
-    //goToLaunchPosition();
-    launcher.startLaunching();
-    launcher.stopLaunching();
-  }
 }
 
