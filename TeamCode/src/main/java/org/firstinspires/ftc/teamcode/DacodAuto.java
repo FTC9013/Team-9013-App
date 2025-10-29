275a8c8ca251ab2e8ef395fc147f50d7f94a1b28
9013Matrix <9013.matrix@gmail.com>
2025-10-29 17:18:55 -0400

    test github access 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DacodAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DacodAuto.java
@@ -29,7 +29,7 @@ public abstract class DacodAuto extends LinearOpMode
   {
     if (amIBlue())
     {
-      return new Pose2d(pose.position.x, pose.position.y * -1, pose.heading.inverse().toDouble());
+      return new Pose2d(pose.position.x, pose.position.y * -1, -pose.heading.toDouble());
     } else
     {
       return pose;
