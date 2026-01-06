1af5c9cc9fdfd880b3668fe15fbef52614a260e7
9013Matrix <9013.matrix@gmail.com>
2026-01-06 17:44:10 -0500

    competition 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeOpMode.java
@@ -23,11 +23,13 @@ public class DecodeOpMode extends LinearOpMode
     
     MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
     boolean wasXPressed = false;
+    boolean wasLeftBumperPressed = false;
     boolean wasBPressed = false;
     boolean wasdpadupPressed = false;
     boolean wasdpaddownPressed = false;
     boolean wasAPressed = false;
     boolean toggleDirection = false;
+    boolean toggleTurbo = true;
     waitForStart();
     
     while (opModeIsActive())
@@ -35,15 +37,33 @@ public class DecodeOpMode extends LinearOpMode
       double gampad1LeftStickY = -gamepad1.left_stick_y;
       double gampad1LeftStickX = -gamepad1.left_stick_x;
       double gampad1RightStickX = -gamepad1.right_stick_x;
+      // toggles the speed and front side of robot
       if (!wasAPressed && gamepad1.a)
       {
         toggleDirection = !toggleDirection;
       }
+      telemetry.addData("Frontside = ", toggleDirection ? "Shooter" : "Intake");
+      
+      
       wasAPressed = gamepad1.a;
+      if (gamepad1.left_bumper && !wasLeftBumperPressed)
+      {
+        toggleTurbo = !toggleTurbo;
+      }
+      telemetry.addData("Turbomode = ", toggleTurbo ? "Slurbo" : "Normal");
+      wasLeftBumperPressed = gamepad1.left_bumper;
+      
+      //flips to the opposite setting
       if (toggleDirection)
       {
         gampad1LeftStickY *= -1;
         gampad1LeftStickX *= -1;
+      }
+      if (toggleTurbo)
+      {
+        gampad1LeftStickY *= 0.67;
+        gampad1LeftStickX *= 0.67;
+        gampad1RightStickX *= 0.67;
       }
       drive.setDrivePowers(new PoseVelocity2d(
         new Vector2d(
@@ -123,18 +143,18 @@ public class DecodeOpMode extends LinearOpMode
         shooter.conveyorBeltP.stopConveying();
         shooter.intake.stopIntaking();
       }
-      if (gamepad1.dpad_up && !wasdpadupPressed)
+      if (gamepad2.dpad_up && !wasdpadupPressed)
       {
         shooter.launchWheelG.launchSpeedIncreasing();
         shooter.launchWheelP.launchSpeedIncreasing();
-        telemetry.addData("Increasing launch speed", gamepad1.dpad_up);
+        telemetry.addData("Increasing launch speed", gamepad2.dpad_up);
       }
       wasdpadupPressed = gamepad1.dpad_up;
-      if (gamepad1.dpad_down && !wasdpaddownPressed)
+      if (gamepad2.dpad_down && !wasdpaddownPressed)
       {
         shooter.launchWheelG.launchSpeedDecreasing();
         shooter.launchWheelP.launchSpeedDecreasing();
-        telemetry.addData("Decreasing launch speed", gamepad1.dpad_down);
+        telemetry.addData("Decreasing launch speed", gamepad2.dpad_down);
       }
       wasdpaddownPressed = gamepad1.dpad_down;
       shooter.launchWheelG.outputSpeed();
