b2195c3270a8ccf5b7cff657a47df8d87f43dff7
9013Matrix <9013.matrix@gmail.com>
2026-02-26 17:57:58 -0500

    we are tuning paths for pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,10 +3,12 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.Encoder;
 import com.pedropathing.ftc.localization.constants.ThreeWheelIMUConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 
@@ -16,19 +18,19 @@ public class Constants
     .forwardTicksToInches(.001989436789)
     .strafeTicksToInches(.001989436789)
     .turnTicksToInches(.001989436789)
-    .leftPodY(1)
-    .rightPodY(-1)
-    .strafePodX(-2.5)
+    .leftPodY(4.25)
+    .rightPodY(-4.25)
+    .strafePodX(-3.25)
     .leftEncoder_HardwareMapName("leftFront")
-    .rightEncoder_HardwareMapName("rightRear")
-    .strafeEncoder_HardwareMapName("rightFront")
+    .rightEncoder_HardwareMapName("rightFront")
+    .strafeEncoder_HardwareMapName("rightRear")
     .leftEncoderDirection(Encoder.FORWARD)
     .rightEncoderDirection(Encoder.FORWARD)
     .strafeEncoderDirection(Encoder.FORWARD)
     .IMU_HardwareMapName("imu")
     .IMU_Orientation(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
-  public static FollowerConstants followerConstants = new FollowerConstants();
-  
+  public static FollowerConstants followerConstants = new FollowerConstants()
+    .mass(5.443);
   public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
   
   public static Follower createFollower(HardwareMap hardwareMap)
@@ -36,6 +38,18 @@ public class Constants
     return new FollowerBuilder(followerConstants, hardwareMap)
       .threeWheelIMULocalizer(localizerConstants)
       .pathConstraints(pathConstraints)
+      .mecanumDrivetrain(driveConstants)
       .build();
   }
+  
+  public static MecanumConstants driveConstants = new MecanumConstants()
+    .maxPower(1)
+    .rightFrontMotorName("rightFront")
+    .rightRearMotorName("rightRear")
+    .leftRearMotorName("leftRear")
+    .leftFrontMotorName("leftFront")
+    .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+    .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+    .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+    .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 }
