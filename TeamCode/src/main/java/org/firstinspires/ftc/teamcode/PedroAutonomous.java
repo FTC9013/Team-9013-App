17e248692573fb993a8f1f9e96ff299a4f50fc62
9013Matrix <9013.matrix@gmail.com>
2026-05-28 16:48:09 -0400

    fixed pathing yo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroAutonomous.java
@@ -5,7 +5,6 @@ import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.follower.Follower;
-import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.ivy.Command;
@@ -66,106 +65,40 @@ public class PedroAutonomous extends OpMode
   
   public static class Paths
   {
-    public PathChain line1;
+    public PathChain Path1;
     public PathChain Path2;
     public PathChain Path3;
-    public PathChain Path4;
-    public PathChain Path5;
-    public PathChain Path6;
-    public PathChain Path7;
-    public PathChain Path8;
-    public PathChain Path9;
     
     public Paths(Follower follower)
     {
-      line1 = follower.pathBuilder().addPath(
+      Path1 = follower.pathBuilder().addPath(
           new BezierLine(
-            new Pose(110.709, 134.992),
+            new Pose(72.553, 8.184),
             
-            new Pose(83.906, 100.630)
+            new Pose(72.332, 71.889)
           )
-        ).setConstantHeadingInterpolation(Math.toRadians(90))
+        ).setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(180))
         
         .build();
       
       Path2 = follower.pathBuilder().addPath(
           new BezierLine(
-            new Pose(83.906, 100.630),
+            new Pose(72.332, 71.889),
             
-            new Pose(96.457, 96.795)
+            new Pose(23.456, 84.009)
           )
-        ).setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(45))
+        ).setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(0))
         
         .build();
       
       Path3 = follower.pathBuilder().addPath(
           new BezierLine(
-            new Pose(96.457, 96.795),
+            new Pose(23.456, 84.009),
             
-            new Pose(104.063, 84.134)
+            new Pose(120.622, 71.724)
           )
-        ).setLinearHeadingInterpolation(Math.toRadians(45), Math.toRadians(0))
+        ).setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(90))
         
-        .build();
-      
-      Path4 = follower.pathBuilder().addPath(
-          new BezierLine(
-            new Pose(104.063, 84.134),
-            
-            new Pose(125.339, 83.677)
-          )
-        ).setTangentHeadingInterpolation()
-        
-        .build();
-      
-      Path5 = follower.pathBuilder().addPath(
-          new BezierLine(
-            new Pose(125.339, 83.677),
-            
-            new Pose(95.827, 96.472)
-          )
-        ).setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(45))
-        
-        .build();
-      
-      Path6 = follower.pathBuilder().addPath(
-          new BezierLine(
-            new Pose(95.827, 96.472),
-            
-            new Pose(101.071, 59.268)
-          )
-        ).setLinearHeadingInterpolation(Math.toRadians(45), Math.toRadians(0))
-        
-        .build();
-      
-      Path7 = follower.pathBuilder().addPath(
-          new BezierLine(
-            new Pose(101.071, 59.268),
-            
-            new Pose(130.260, 59.488)
-          )
-        ).setTangentHeadingInterpolation()
-        
-        .build();
-      
-      Path8 = follower.pathBuilder().addPath(
-          new BezierCurve(
-            new Pose(130.260, 59.488),
-            new Pose(108.380, 65.419),
-            new Pose(96.106, 96.626)
-          )
-        ).setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(45))
-        
-        .build();
-      
-      Path9 = follower.pathBuilder().addPath(
-          new BezierLine(
-            new Pose(96.106, 96.626),
-            
-            new Pose(72.087, 48.268)
-          )
-        ).setTangentHeadingInterpolation()
-        .setReversed()
         .build();
     }
   }
@@ -174,22 +107,12 @@ public class PedroAutonomous extends OpMode
   public Command autoRoutine()
   {
     return sequential(
-      follow(follower, paths.line1),
+      follow(follower, paths.Path1),
       /* Score Preload Command*/
       follow(follower, paths.Path2, true),
       /* Grab Sample Command*/
-      follow(follower, paths.Path3, true),
-      /* Score Sample Command*/
-      follow(follower, paths.Path4, true),
-      /* Grab Sample Command*/
-      follow(follower, paths.Path5, true),
-      /* Score Sample Command*/
-      follow(follower, paths.Path6, true),
-      /* Grab Sample Command*/
-      follow(follower, paths.Path7, true),
-      follow(follower, paths.Path8, true),
-      follow(follower, paths.Path9, true)
-      /* Score Sample Command*/
+      follow(follower, paths.Path3, true)
+    
     );
   }
 }
